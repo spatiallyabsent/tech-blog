17686888ecbae671bb9e3a87f6d5ef0e97496270
SpatiallyAbsent <joshuapdow@gmail.com>
2024-02-02 14:11:53 -0700

    finished off models/User.js unless something else pops up

--- models/User.js
+++ models/User.js
@@ -1,41 +1,63 @@
 const { Model, DataTyoes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const Sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model { }
 
 User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
     username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
     },
     email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true // Ensure the value is a valid email address
-      }
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true // Ensure the value is a valid email address
+        }
     },
     password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: {
-          args: [8, Infinity], 
-          msg: 'Password must be at least 8 characters long'
-        },
-        containsSymbol(value) {
-          if (!/\W/.test(value)) { 
-            throw new Error('Password must contain at least one symbol');
-          }
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: {
+                args: [8, Infinity],
+                msg: 'Password must be at least 8 characters long'
+            },
+            containsSymbol(value) {
+                if (!/\W/.test(value)) {
+                    throw new Error('Password must contain at least one symbol');
+                }
+            }
         }
-      }
     },
-    // add more here as we fill out more info. Delete this message later
-  }, {
-    sequelize,
-    modelName: 'user'
-  });
-  
-  module.exports = User;
+},
+    {
+        hooks: {
+            beforeCreate: async (user) => {
+                user.password = await bcrypt.hash(user.password, 10);
+                return user;
+            },
+            beforeUpdate: async (user) => {
+                if (user.changed('password')) {
+                    user.password = await bcrypt.hash(user.password, 10);
+                }
+                return user;
+            },
+        },
+        sequelize,
+        modelName: 'user',
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+    });
+
+module.exports = User;
