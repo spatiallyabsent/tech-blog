21e7445249a332901e66affaa735396e00082736
SpatiallyAbsent <joshuapdow@gmail.com>
2024-02-03 17:51:47 -0700

    added the route for dashboard into index.js under controllers

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homePageRoute = require('./homepageRoute');
+const dashboardRoute = require('./dashboardRoute');
 //still need to add the other api routes
 
 router.use('/', homePageRoute);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoute);
 //still need to add the other api routes
 
 module.exports = router;
