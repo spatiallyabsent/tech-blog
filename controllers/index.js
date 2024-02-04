d885b6c0795e26e3263cab8f684ebffb49a3b5c6
SpatiallyAbsent <joshuapdow@gmail.com>
2024-02-04 13:52:48 -0700

    added commentsRoute to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,14 @@ const apiRoutes = require('./api');
 const homePageRoute = require('./homepageRoute');
 const dashboardRoute = require('./dashboardRoute');
 const postsRoute = require('./postsRoute');
+const commentsRoute = require('./commentsRoute');
 //still need to add the other api routes
 
 router.use('/', homePageRoute);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoute);
-router.use('./postsRoute', postsRoute);
+router.use('./posts', postsRoute);
+router.use('./comments', commentsRoute);
 //still need to add the other api routes
 
 module.exports = router;
